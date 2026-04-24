c8f1680a2bfd685c5d11a531b5ae712f03c955e6
kibria <shan2neo@gmail.com>
2026-04-24 10:59:45 +0200

    feat: integrate LangChain, Google Generative AI, and FAISS for PDF processing capabilities

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+import PyPDF2
 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
 app = Flask(__name__)
 
 @app.route('/')
